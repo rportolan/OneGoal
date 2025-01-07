503a02a92766e32c9b0454bbdc1c214c416a695e
rportolan <romeoprtl.dev@gmail.com>
2025-01-07 10:55:27 +0100

    Update des cards statistiques

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -5,7 +5,7 @@ import NotificationsIcon from "../molecules/NotificationIcon";
 const Header = () => {
     return(
         <>
-            <header className="flex bg-primary m-2 rounded-xl items-center pb-4 pt-4 justify-between">
+            <header className="flex bg-primary mt-2 ml-2 mb-2 mr-4 rounded-xl items-center pb-4 pt-4 justify-between">
                 <LogoOneGoal />
                 <div className="flex items-center">
                 <div className="mr-8">
