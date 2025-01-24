2c08d7ca060505c0b96f7949a5e02512f58be2e7
rportolan <romeoprtl.dev@gmail.com>
2025-01-24 11:38:03 +0100

    Sauvegarde des modifications de Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -26,10 +26,11 @@ const Analytics = () => {
   return (
     <>
         <h2 className='ml-4 py-9 font-bold'>Statistiques</h2>
-            <div className="ml-4 flex">
-                <div className="border border-secondary rounded-lg h-56 w-56 flex flex-col justify-between items-center">
-                    <h3 className="text-sm m-4">Nombre total d'objectifs réalisé</h3>
-                    <span className="text-7xl font-extrabold flex-1 mb-10 flex items-center justify-center">30</span>
+        <div className="ml-6 mr-6 flex flex-wrap">
+                <div className=" flex flex-wrap w-full justify-between mb-9">
+                    <CardStat titleCard="Nombre total d'objectifs réalisés" value="14" isActive={true} progress="+ 4" descStat="Nombre total d'objectif réalisés depuis la création de votre compte, la progression repose sur le mois dernier."/>
+                    <CardStat titleCard="Nombre total de sous-objectifs réalisés" value="79" isActive={true} progress="+ 30" descStat="Nombre total de sous-objectifs réalisés depuis la création de votre compte, la progression repose sur le mois dernier."/>
+                    <CardStat titleCard="Nombre total de tâches réalisé" value="289" isActive={true} progress="+ 90" descStat="Nombre total de tâches réalisés depuis la création de votre compte, la progression repose sur le mois dernier."/>
                 </div>
                 <div className="flex flex-wrap w-full justify-between mb-8">
                     <CardStat titleCard="Temps total travaillé" value="94:48:12" isActive={false} progress="- 5" descStat="Temps total travaillé depuis la création de votre compte, il repose sur le temps que vous définis sur le chronomètre. La progression repose sur le mois dernier."/>
